21fc40dedc1ad5315647aba0da70cf62a8e77ab5
Pavel <virus191288@gmail.com>
2018-06-10 18:37:36 +0500

    add build

---
+++ docs/precache-manifest.0cbfb9ca205ea98b218d119d4e329059.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a27c909c851f11a3d0dd07633e6b3420",
+    "url": "index.html"
+  },
+  {
+    "revision": "b1671b4c3a8e862d21b7",
+    "url": "chunk-vendors.b1671b4c.js"
+  },
+  {
+    "revision": "570a034a5ebebb96991a",
+    "url": "app.eef60fba.css"
+  },
+  {
+    "revision": "570a034a5ebebb96991a",
+    "url": "app.570a034a.js"
+  }
+];
